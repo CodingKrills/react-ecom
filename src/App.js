2daf2af3a1832178f92a55707355c05a60f6274f
codingKrills <pulkitsinghmail@gmail.com>
2021-04-07 15:58:38 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="/">Dashboard</a>
+              <hr />
             </li>
             <li className="nav-item">
               <a className="nav-link" href="/">Notifications</a>
