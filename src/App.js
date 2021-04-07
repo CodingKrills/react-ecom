d8ffcdc360a35e15c708177ff1f52a5790536913
codingKrills <pulkitsinghmail@gmail.com>
2021-04-07 15:37:00 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light shadow" aria-label="Main navigation">
       <div className="container">
-        <a className="navbar-brand" href="#">Offcanvas navbar</a>
+        <a className="navbar-brand" href="/">Offcanvas navbar</a>
         <button className="navbar-toggler mt-2 p-0 border-0 nav-icon0" type="button" onClick={openNav}>
           <span></span>
           <span></span>
@@ -25,23 +25,23 @@ function App() {
         <div className="navbar-collapse offcanvas-collapse" >
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Dashboard</a>
+              <a className="nav-link active" aria-current="page" href="/">Dashboard</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Notifications</a>
+              <a className="nav-link" href="/">Notifications</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Profile</a>
+              <a className="nav-link" href="/">Profile</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Switch account</a>
+              <a className="nav-link" href="/">Switch account</a>
             </li>
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">Settings</a>
+              <a className="nav-link dropdown-toggle" href="/" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">Settings</a>
               <ul className="dropdown-menu" aria-labelledby="dropdown01">
-                <li><a className="dropdown-item" href="#">Action</a></li>
-                <li><a className="dropdown-item" href="#">Another action</a></li>
-                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                <li><a className="dropdown-item" href="/">Action</a></li>
+                <li><a className="dropdown-item" href="/">Another action</a></li>
+                <li><a className="dropdown-item" href="/">Something else here</a></li>
               </ul>
             </li>
           </ul>
