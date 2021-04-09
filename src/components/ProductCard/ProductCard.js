5e8fdc609ab551e6a760404d9663c18005df4721
codingKrills <pulkitsinghmail@gmail.com>
2021-04-09 14:36:51 +0530

    updates

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -61,14 +61,14 @@ const ProductCard = () => {
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 3,
                     slidesToScroll: 1,
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 3,
                     slidesToScroll: 1
                 }
             }
